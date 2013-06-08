6ffed5363c25ada42bfd4af53edc424f7948c52c
Samson Jin <apsong@gmail.com>
2013-06-08 00:12:39 -0700

    1

---
+++ trunk/HSIM/iHub/Transient/run.bat
@@ -0,0 +1,1 @@
+..\..\bin\HsimD.exe -testType SSO -H <HOST> -port 8900 -soapserver <HOST> -soapport 8900 -clients 4 -l 86400 -d iportal -S <VOLUMN> SampleBIRTReport.sim
---
+++ trunk/HSIM/iHub/Transient/sanity.bat
@@ -0,0 +1,1 @@
+..\..\bin\HsimD.exe -testType SSO -H <HOST> -port 8900 -soapserver <HOST> -soapport 8900 -clients 1 -n 1 -d iportal -S <VOLUMN> SampleBIRTReport.sim -v
