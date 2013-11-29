ec49a6ea1560aa0f4000033611e835c37f632cb9
Samson Jin <apsong@gmail.com>
2013-11-29 05:24:58 +0000

    ps1

--- trunk/.JS/ps1/iHubDownload.ps1
+++ trunk/.JS/ps1/iHubDownload.ps1
@@ -4,29 +4,28 @@ function iHubDownload ($release_base, $license_base, $DST_DIR) {
     $SRC_DIR = (Get-ChildItem "$release_base\DEV-*" | Sort-Object | Select-Object -last 1).FullName
     $DATE = $SRC_DIR -replace ".*-DEV", ""
 
-    #$CSV = $XLS.Replace(".xlsx", ".csv")
-    $SRC = "${SRC_DIR}\iHub3\modules\ActuateBIRTiHub.zip"
-
-    $DST_NAME = "ActuateBIRTiHub${DATE}"
-    $DST = "${DST_DIR}\${DST_NAME}.zip"
+    $SRC = (Get-ChildItem $SRC_DIR -Recurse -Filter ActuateBIRTiHub.zip).FullName
+    
+    $DST_FILE = "ActuateBIRTiHub${DATE}"
+    $DST = "${DST_DIR}\${DST_FILE}.zip"
     Get-ChildItem $SRC
 
     if (Test-Path $DST) {
-        "`nINFO: $DST already exists."
+        "`nNOTE: $DST already exists."
         exit 0
     }
     Copy-Item $SRC $DST
     Get-ChildItem $DST
 
-    unzip -q $DST -d $DST_DIR\$DST_NAME
-    ls $DST_DIR\$DST_NAME
+    unzip -q (cygpath $DST) -d (cygpath $DST_DIR\$DST_FILE)
+    ls $DST_DIR\$DST_FILE
 
-    $LIC_ORIG = "$DST_DIR\$DST_NAME\iHub\shared\config\acserverlicense.xml"
+    $LIC_ORIG = "$DST_DIR\$DST_FILE\iHub\shared\config\acserverlicense.xml"
     if (-not (Test-Path "$LIC_ORIG.orig")) {
         mv $LIC_ORIG "$LIC_ORIG.orig"
         cp "$license_base\acserverlicense-autotest.xml" $LIC_ORIG
     }
-    ls "$LIC_ORIG*"
+    ls "$LIC_ORIG*" | Select-String "UsageType|ExpirationDate" | %{ $_.Path + ":" + $_.LineNumber + "`t" + $_.Line}
 }
 
 $HOSTNAME = (hostname)
